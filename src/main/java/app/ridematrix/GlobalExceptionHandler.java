9a4ed39fb0d3eef882c3d8cd0da35a09553dbdd7
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-30 20:36:54 +0530

    VEHMS-M01-T007: Global Exception added For validation

---
+++ src/main/java/app/ridematrix/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package app.ridematrix;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler
+{
+    //Handle validation error
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Add mandatory fields");
+    }
+}
