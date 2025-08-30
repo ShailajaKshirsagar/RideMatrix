c340c945c5953577e75f4bd16ff8210a50d04d30
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-31 00:17:23 +0530

    VEHMS-M01-T0012: Getresidentbyname API Implemented with validation

--- src/main/java/app/ridematrix/repository/ResidentRepo.java
+++ src/main/java/app/ridematrix/repository/ResidentRepo.java
@@ -10,11 +10,14 @@ import java.util.List;
 public interface ResidentRepo extends JpaRepository<Resident,Integer>
 {
     //get resident by name jpql query
-    @Query("SELECT r FROM Resident r WHERE " +
-            "(:fName IS NULL OR r.fName = :fName) AND " +
-            "(:lName IS NULL OR r.lName = :lName)")
+    //Ilike -> ignore cases (captal,small)
+    @Query(value = "SELECT * FROM resident r WHERE " +
+            "(:fName IS NULL OR r.f_name ILIKE :fName) AND " +
+            "(:lName IS NULL OR r.l_name ILIKE :lName)", nativeQuery = true)
     List<Resident> findResidentByName(
             @Param("fName") String fName,
             @Param("lName") String lName
     );
+
+
 }
