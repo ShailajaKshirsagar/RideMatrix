40f8f05de2e6a8c2c8db1aca0084d8c4c7af12ce
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-30 17:49:45 +0530

    VEHMS-M01-T001 : Created RideMatrixSpring Boot project

---
+++ src/test/java/app/ridematrix/RideMatrixVmsApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ridematrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideMatrixVmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
