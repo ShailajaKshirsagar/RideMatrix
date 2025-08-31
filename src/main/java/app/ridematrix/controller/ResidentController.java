7e3422b14f57c438af48799cd8607e98a5d20ea6
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-08-31 13:05:15 +0530

    VEHMS-M01-T009 : Swagger documentation Tested with proper api msg.

--- src/main/java/app/ridematrix/controller/ResidentController.java
+++ src/main/java/app/ridematrix/controller/ResidentController.java
@@ -2,6 +2,9 @@ package app.ridematrix.controller;
 
 import app.ridematrix.entity.Resident;
 import app.ridematrix.service.ResidentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -15,6 +18,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/residents")
+@Tag(name = "Resident Controller", description = "APIs to manage residents and their vehicles")
+//Tag for swagger api documentation
 public class ResidentController
 {
     @Autowired
@@ -22,6 +27,7 @@ public class ResidentController
 
     //API to create Resident with vehicals
     @PostMapping("/addResident")
+    @Operation(summary = "Create a new resident with vehicles")
     public ResponseEntity<String> createResident(@Valid @RequestBody Resident resident){
         String msg = residentService.saveResident(resident);
         return new ResponseEntity<>(msg,HttpStatus.CREATED);
@@ -29,6 +35,7 @@ public class ResidentController
 
     //API to get all resident along with their vehicle
     @GetMapping("/getAllResidents")
+    @Operation(summary = "Get all Residents details with Vehicles")
     public ResponseEntity<List<Resident>> getAllResident(){
         List<Resident> residentList = residentService.getAllResidents();
         return new ResponseEntity<>(residentList,HttpStatus.OK);
@@ -37,10 +44,13 @@ public class ResidentController
     //API to get resident details with name
     // ? -> means it can either return name or string msg
     @GetMapping("/getResidentByName")
+    @Operation(summary = "Get Resident details with name")
     public ResponseEntity<?> getResidentByName(
             @Pattern(regexp = "^[A-Za-z]*$", message = "First name should not contain numbers")
+            @Parameter(description = "First name of resident (optional)")
             @RequestParam(required = false) String fName ,
             @Pattern(regexp = "^[A-Za-z]*$", message = "Last name should not contain numbers")
+            @Parameter(description = "Last name of resident (optional)")
             @RequestParam(required = false) String lName)
     {
         List<Resident> residentByName = residentService.getResidentByName(fName, lName);
