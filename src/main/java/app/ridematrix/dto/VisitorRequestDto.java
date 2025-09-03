0bc6aafce87659b77c2b2dc69bf7c3bf9a9ff25d
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-03 23:03:54 +0530

    VEHMS-M02-T021 :API- GET - Visitors details by registeration number

--- src/main/java/app/ridematrix/dto/VisitorRequestDto.java
+++ src/main/java/app/ridematrix/dto/VisitorRequestDto.java
@@ -2,6 +2,8 @@ package app.ridematrix.dto;
 
 import app.ridematrix.entity.Visitors;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,10 +14,17 @@ import java.time.LocalDateTime;
 public class VisitorRequestDto
 {
 
+        @NotNull(message = "Visitor name is required")
         private String visitorName;
         private String vehicleName;
+        @NotNull(message = "Registration Number is Required")
+         @Pattern(
+            regexp = "^[A-Z0-9]{4}-[A-Z]{2}-\\d{4}$",
+            message = "Registration number must be in format XX-00-XX-0000 equal to 10"
+        )
         private String vehicleRegNumber;
         private String visitPurpose;
+        @NotNull(message = "Phone number is required")
         private long phoneNum;
         @JsonProperty(access = JsonProperty.Access.READ_ONLY)
         private LocalDateTime timeIn;
--- src/main/java/app/ridematrix/service/VisitorService.java
+++ src/main/java/app/ridematrix/service/VisitorService.java
@@ -1,9 +1,15 @@
 package app.ridematrix.service;
 
 import app.ridematrix.dto.VisitorRequestDto;
+import app.ridematrix.dto.VisitorResponseDTO;
 import jakarta.validation.Valid;
 
 public interface VisitorService
 {
     String saveVisitor(@Valid VisitorRequestDto visitors);
+
+    VisitorResponseDTO findVisitorResidentByRegNum(String vehicleRegNum);
+
+    //update out time
+    String updateOutTime(String vehicleRegNum);
 }
