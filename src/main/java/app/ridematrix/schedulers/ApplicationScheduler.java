a0fe751db01b745b161cca2c8b57ea5bae53894e
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-06 22:09:29 +0530

    VEHMS-M02-T031 : Schedular - to store Backup of visitor's history

--- src/main/java/app/ridematrix/schedulers/ApplicationScheduler.java
+++ src/main/java/app/ridematrix/schedulers/ApplicationScheduler.java
@@ -1,6 +1,9 @@
 package app.ridematrix.schedulers;
 
+import app.ridematrix.converter.VisitorExcelDataMapper;
+import app.ridematrix.dto.VisitorExcelDataDTO;
 import app.ridematrix.entity.Visitors;
+import app.ridematrix.helper.ExportVisitorsToExcel;
 import app.ridematrix.repository.VisitorRepo;
 import jakarta.persistence.Converter;
 import jakarta.transaction.Transactional;
@@ -8,7 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ApplicationScheduler
@@ -18,12 +28,43 @@ public class ApplicationScheduler
 
     //scheduler to find visitduration field null and set it
     @Transactional
-    @Scheduled(fixedRate = 6000)
+    @Scheduled(cron = "0 0 0 * * ?")
+    //@Scheduled(fixedRate = 6000)
     public void setVisitDurationIfNull(){
         List<Visitors> visitorsList = visitorRepository.findVisitorWithNullVisitDuration();
         visitorsList.forEach(v -> {
             v.setVisitDuration(v.calculateVisitDuration());
             visitorRepository.save(v);  // Save changes to DB
         });
+    }
+
+    //Scheduler to save backup of visitor data in excel on local drive
+    @Scheduled(fixedRate = 6000)
+    public void visitorDataInExcel() {
+        try {
+            List<Visitors> visitors = visitorRepository.findAll();
+            List<VisitorExcelDataDTO> visitorExcelDataDTOList = visitors.stream()
+                    .map(VisitorExcelDataMapper::toDto)
+                    .collect(Collectors.toList());
+
+            String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("ddMMyyyy"));
+
+            String folderPath = "D:/visitors_log";
+            File exportFolder = new File(folderPath);
+            if (!exportFolder.exists()) {
+                exportFolder.mkdirs();
+                //create folder
+            }
+
+            String filePath = folderPath + "/visitors_history_log" + currentDate + ".xlsx";
+            try (FileOutputStream fos = new FileOutputStream(filePath)) {
+                ExportVisitorsToExcel.visitorsToExcel(visitorExcelDataDTOList, fos);
+            } // fos auto closed here
+
+            System.out.println("Visitor Excel exported: " + filePath);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
