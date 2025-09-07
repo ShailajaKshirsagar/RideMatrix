d2d12067cc1603d42cf2c58f332ff7a7ddb51baf
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-07 15:38:41 +0530

    VEHMS-M02-T032 : Testing Global Exception Added

---
+++ src/main/java/app/ridematrix/exception/MethodArgumentTypeMismatchException.java
@@ -0,0 +1,8 @@
+package app.ridematrix.exception;
+
+public class MethodArgumentTypeMismatchException extends RuntimeException
+{
+    public MethodArgumentTypeMismatchException(String message) {
+        super(message);
+    }
+}
