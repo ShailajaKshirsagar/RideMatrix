7daedcbe3382763992dc7c59cfa97dfd9dc5e20e
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-09-07 15:40:27 +0530

    VEHMS-M02-T032 : Testing Global Exception Added

--- src/main/java/app/ridematrix/GlobalExceptionHandler.java
+++ src/main/java/app/ridematrix/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package app.ridematrix;
 
-import app.ridematrix.exception.InvalidRegistrationNum;
-import app.ridematrix.exception.ResourceNotFoundException;
+import app.ridematrix.exception.*;
+import app.ridematrix.exception.IllegalArgumentException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -42,6 +44,35 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> HandleDatabaseException(DataAccessException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Database operation failed. Please try again later.");
+    }
+
+    //To handle Illegal argument Exception
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArguException(IllegalArgumentException ex){
+        return ResponseEntity.badRequest().body("Invalid input: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String msg = "Invalid parameter type provided: " + ex.getMessage();
+        return ResponseEntity.badRequest().body(msg);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<String> handleMissingParams(MissingServletRequestParameterException ex) {
+        String msg = "Required request parameter is missing" + ex.getMessage();
+        return ResponseEntity.badRequest().body(msg);
+    }
+
+    //validation failures handle
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolation(ConstraintViolationException ex) {
+        String message = ex.getConstraintViolations()
+                .stream()
+                .map(cv -> cv.getMessage())  // this is correct
+                .findFirst()
+                .orElse("Validation failed");
+        return ResponseEntity.badRequest().body("Validation error: " + message);
     }
 }
 
