c0088429e15d72eff9c0cf00fb59f3c4e58ffb11
Shailaja Kshirsagar <shailajakshirsagarr@gmail.com>
2025-11-30 19:46:24 +0530

    file updated

--- src/main/java/app/ridematrix/GlobalExceptionHandler.java
+++ src/main/java/app/ridematrix/GlobalExceptionHandler.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
+//exception handler
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
